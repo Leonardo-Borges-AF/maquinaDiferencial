79e9191ac82096a5f45c6d5e303a59de374afcde
Leonardo Borges <leoborgesaf@hotmail.com>
2024-10-24 16:36:12 -0300

    fix/adjustments

--- main.py
+++ main.py
@@ -28,9 +28,9 @@ def encontraLinhaValorIgual(diffs):
             return linha[0]
 
 
-coeficientes = [2,-3,1,-5]
+coeficientes = [2,-3,2]
 grau = len(coeficientes) - 1
-eixo_x = [1,2,3,4,5,6,7,8,9]
+eixo_x = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8]
 eixo_y = [polinomio(x, coeficientes, grau) for x in eixo_x]
 
 diffs = babbage(eixo_y)
--- teste.py
+++ teste.py
@@ -4,7 +4,7 @@ def babbage(eixo_y):
     for i in range(n - 1):
         linha = []
         for j in range(n - i - 1):
-            v = round(tabela[i][j] - tabela[i][j + 1], 2) 
+            v = round(tabela[i][j] - tabela[i][j + 1], 4) 
             linha.append(v)
         tabela.append(linha)
     return tabela
@@ -18,42 +18,49 @@ def polinomio(x, coeficientes, grau):
     return soma
 
 
-def proximoA(grau, tabela):
-    
+def proximoA(grau, tabela, repete):
+    aux = grau
+    while repete > 0:
+        grau = aux
+        while grau > 0:
+            resultado = round(tabela[grau-1][-1] - tabela[grau][-1],4)
+            tabela[grau-1].append(resultado)
+            grau -=1
+        repete -=1
+    grau = aux
     while grau > 0:
-
-        resultado = tabela[grau-1][-1] - tabela[grau][-1]
- 
+        resultado = round(tabela[grau-1][-1] - tabela[grau][-1],4)
         tabela[grau-1].append(resultado)
-
         grau -=1
-        
-    return resultado
+    return tabela , resultado
 
-        
+def calculaRepeticao(eixo_x, numero):
+    diferença = eixo_x[2] - eixo_x[1]
+    repete = (numero - eixo_x[-1]) / diferença
+    print(diferença)
+    return repete
+
 def encontraLinhaValorIgual(diffs):
     for linha in diffs:
         if all(x == linha[0] for x in linha):
             return linha[0]
-    
 
-
-coeficientes = [2,-3,1,-5]
+coeficientes = [2,-3,2]
 grau = len(coeficientes) - 1
-eixo_x = [1,2,3,4,5,6,7,8,9]
+eixo_x = [0.1,0.2,0.3,0.4]
 eixo_y = [polinomio(x, coeficientes, grau) for x in eixo_x]
 
 diffs = babbage(eixo_y)
 valorIgual = encontraLinhaValorIgual(diffs)
-
-# proximo = proximoA(diffs)
-
+repete = calculaRepeticao(eixo_x, 0.9)
 
 print(eixo_x)
 print(eixo_y)
-print("tabela de diferenças:")
-for c, linha in enumerate(diffs):
-    print(f"nível {c}: {linha}")
-# print(f"Próximo valor estimado na sequência: {proximo}")
 
-print("proximo valor sera de:", proximoA(grau, diffs))
+tabela , resultado = proximoA(grau, diffs, repete)
+print("tabela de diferenças:")
+for c, linha in enumerate(tabela):
+    print(f"nível {c}: {linha}")
+
+
+print("proximo valor sera de:", resultado)
